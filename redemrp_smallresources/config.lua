6463f8f90aa0b4085af588c2cad35fbffa96e445
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-09 11:05:46 -0700

    Update config.lua

--- redemrp_smallresources/config.lua
+++ redemrp_smallresources/config.lua
@@ -1,11 +1,11 @@
 Trigger = {
     HANDSUP = true, -- Enables the handsup emote by using the /hu command
-    BATHING = false, -- Enable bath houses
+    BATHING = true, -- Enable bath houses
     BLIPS = true, -- Create blips for locations
     COORDS = true, -- Displays player coordinates on screen by using /coords
-    PLAYERID = false, -- Toggles player ID abvove head with Z
+    PLAYERID = true, -- Toggles player ID abvove head with Z
     GUNTWIRL = true, -- Gun tricks
-    POINTING = false, -- Press B to point
+    POINTING = true, -- Press B to point
 
 --**WARNING**----------Beta------------------**WARNING**----------Beta------------------**WARNING**----------Beta------------------**WARNING**
     FULLCORES = false, -- Start with full Health, stamina and deadeye **WARNING** Beta Mode
