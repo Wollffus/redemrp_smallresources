ecd82836dac782eea4e7bec641286f601e01bde1
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-25 11:38:25 -0700

    Update pointing.lua

--- redemrp_smallresources/client/pointing.lua
+++ redemrp_smallresources/client/pointing.lua
@@ -1,4 +1,5 @@
 -- -- Point  -------------------------------------------------------------------------
+if Trigger.Point then
 Citizen.CreateThread(function()
     while true do
         Wait(0)
@@ -21,3 +22,4 @@ Citizen.CreateThread(function()
         end
     end
 end)
+end
