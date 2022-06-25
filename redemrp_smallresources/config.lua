4c0580f7db7e74c1db7db277f2d8a44fb3d4af85
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-25 11:40:05 -0700

    Update config.lua

--- redemrp_smallresources/config.lua
+++ redemrp_smallresources/config.lua
@@ -1,10 +1,15 @@
 Trigger = {
-    HANDSUP = true, -- Enables the handsup emote by using the /hu command
+    AimAssist = true, -- Disables aimassist when true, Enables when false
     BATHING = true, -- Enable bath houses
     BLIPS = true, -- Create blips for locations
     COORDS = true, -- Displays player coordinates on screen using command /coords
+    DeleteHorse = true, -- Use command /dh to delete abondoned horses
+    DoorOpener = true, -- Opens doors in buildings around the map. Set to false if you use another
+    GUNTWIRL = true, -- Gun tricks using command /guntricks
+    HANDSUP = true, -- Enables the handsup emote by using the /hu command
     PLAYERID = true, -- Toggles player ID abvove head with Z
-    STAMINA = true, -- Gold ring stamina bar
-    GUNTWIRL = true, -- Gun tricks
-    PvPToggle = true -- Toggle passive mode using commands /pvpon /pvpoff
+    Point = true, -- Use `B` to point (line 6 pointing.lua to change default button)
+    PvPToggle = true, -- Toggle passive mode using commands /pvpon /pvpoff
+    Smoking = true, -- Enable smoking animations
+    STAMINA = true -- Gold ring stamina bar
 }
