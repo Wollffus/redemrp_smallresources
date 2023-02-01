c51e3873e6888e01ec27121bb7a2360c583e284f
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-01-31 18:06:54 -0800

    Update displayjob.lua

--- redemrp_smallresources/server/displayjob.lua
+++ redemrp_smallresources/server/displayjob.lua
@@ -1,7 +1,11 @@
+RedEM = exports["redem_roleplay"]:RedEM()
+
 RegisterCommand('showjob', function(source)
     local _source = source
-    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-        local job = user.getJob()
+    local Player = RedEM.GetPlayer(_source)
+        local job = Player.getJob()
+        local jobgrade = Player.getJobgrade()
         TriggerClientEvent('redem_roleplay:Tip', _source, job, 3000)
+        Wait(3000)
+        TriggerClientEvent('redem_roleplay:Tip', _source, jobgrade, 3000)
     end)
-end)
