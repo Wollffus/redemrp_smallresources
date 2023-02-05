bc7c27aebd8c94ac56592ea767d8b299980214ae
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-02-04 23:20:48 -0800

    Delete redemrp_smallresources/client directory

--- redemrp_smallresources/client/aimassist.lua
+++
@@ -1,9 +0,0 @@
--- -- Disable controller aim assist -------------------------------------------------------------------------
-if Trigger.AimAssist then
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(10)
-        SetPlayerTargetingMode(3) -- or Citizen Native : Citizen.InvokeNative(0xD66A941F401E7302, 3)
-    end
-end)
-end
